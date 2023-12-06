05658899cb70f2fb4dafec5aeac1ea76e7a03691
H-server <hani103201@naver.com>
2023-12-06 14:25:05 +0900

    refactor: OutputView 스트라이크, 볼 출력 로직 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -14,18 +14,19 @@ public class OutputView {
     public static void printAttemptResult(Map<String, Integer> result) {
         int strike = result.get("strike");
         int ball = result.get("ball");
-        if(ball > 0 && strike == 0) {
-            System.out.println(ball + "볼");
+        if (ball > 0) {
+            System.out.print(ball + "볼");
+            if (strike > 0) {
+                System.out.print(" ");
+            }
         }
-        if(ball > 0 && strike > 0) {
-            System.out.println(ball + "볼" + " " + strike + "스트라이크");
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
         }
-        if(ball == 0 && strike > 0) {
-            System.out.println(strike + "스트라이크");
+        if (strike == 0 && ball == 0) {
+            System.out.print("낫싱");
         }
-        if(strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-        }
+        System.out.println();
     }
 
     public static void printGameExit() {
